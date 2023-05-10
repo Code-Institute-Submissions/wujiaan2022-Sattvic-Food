49dcdf5b327aa558881b58d29157d7325e9a3ac1
wujiaan2022 <emmawu2004@gmail.com>
2023-05-10 04:18:15 +0000

    deployment part 2

--- Sattvic_Food_Proj/settings.py
+++ Sattvic_Food_Proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-qr6!r%s=2_t8b666l)idi@o7v_lyuw^$lo$v7*q$w3td-e22n-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sattvic-food.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -117,14 +118,20 @@ WSGI_APPLICATION = 'Sattvic_Food_Proj.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
