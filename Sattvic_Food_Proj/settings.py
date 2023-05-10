c22de785856a7a42a04f495ce1669c7985c15f42
wujiaan2022 <emmawu2004@gmail.com>
2023-05-10 16:13:10 +0000

    reset debug

--- Sattvic_Food_Proj/settings.py
+++ Sattvic_Food_Proj/settings.py
@@ -22,10 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+if os.path.exists("env.py"):
+    import env
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
+
 
 ALLOWED_HOSTS = ['sattvic-food.herokuapp.com', 'localhost']
 
