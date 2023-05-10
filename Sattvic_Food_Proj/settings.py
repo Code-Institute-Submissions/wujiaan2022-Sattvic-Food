74d10a93084d6dc1a1dfad75d9794e0d0b86dd09
wujiaan2022 <emmawu2004@gmail.com>
2023-05-10 05:18:58 +0000

    removed secret key and set the debug

--- Sattvic_Food_Proj/settings.py
+++ Sattvic_Food_Proj/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qr6!r%s=2_t8b666l)idi@o7v_lyuw^$lo$v7*q$w3td-e22n-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sattvic-food.herokuapp.com', 'localhost']
 
