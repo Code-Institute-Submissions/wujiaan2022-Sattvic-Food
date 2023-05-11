6b6e13791c9c3c131688b4b6c768a958e761a7ec
wujiaan2022 <emmawu2004@gmail.com>
2023-05-11 21:29:03 +0000

    saved a unsaved file

--- Sattvic_Food_Proj/settings.py
+++ Sattvic_Food_Proj/settings.py
@@ -28,8 +28,8 @@ if os.path.exists("env.py"):
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 
 ALLOWED_HOSTS = ['sattvic-food.herokuapp.com', 'localhost']
