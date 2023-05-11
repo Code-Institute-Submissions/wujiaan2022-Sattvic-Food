4f999a72922c1b770597fb9469bf2eac7a231048
wujiaan2022 <emmawu2004@gmail.com>
2023-05-11 07:15:01 +0000

    try to fix products not loading

--- Sattvic_Food_Proj/settings.py
+++ Sattvic_Food_Proj/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-# if os.path.exists("env.py"):
-#     import env
+if os.path.exists("env.py"):
+    import env
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['sattvic-food.herokuapp.com', 'localhost']
