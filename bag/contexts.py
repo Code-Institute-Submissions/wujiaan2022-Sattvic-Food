2103980b163ea6291aad6526c979a61fa4f4e2ce
wujiaan2022 <emmawu2004@gmail.com>
2023-10-14 14:02:01 +0000

    modified the contexts.py and templates to show item name, size, and image in shopping bag page after changed the product model

--- bag/contexts.py
+++ bag/contexts.py
@@ -13,23 +13,24 @@ def bag_contents(request):
     
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
-            product = get_object_or_404(ProductSize, pk=item_id)
-            total += item_data * product.price
+            product_with_size = get_object_or_404(ProductSize, pk=item_id)
+            product = product_with_size.product
+            total += item_data * product_with_size.price
             product_count += item_data
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
-                'product': product,
+                'product_with_size': product_with_size,
             })
         else:
-            product = get_object_or_404(ProductSize, pk=item_id)
+            product_with_size = get_object_or_404(ProductSize, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
+                total += quantity * product_with_size.price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
-                    'product': product,
+                    'product_with_size': product_with_size,
                     'size': size,
                 })
 
